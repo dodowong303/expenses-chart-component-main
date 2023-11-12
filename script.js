5d29d8016d9a22d5a208518a9057fcf3f0da4143
Edmond <ahneineinei@gmail.com>
2023-11-12 17:38:23 -0500

    Layout updates
     - Update fonts
     - Update bar table (add interactions, auto updates)
     - Update layouts

--- script.js
+++ script.js
@@ -38,19 +38,65 @@ const getData = async () => {
   return data;
 };
 
+const getDevice = () => {
+  const width = screen.width;
+  if (width >= 768) return "web";
+  if (width < 768) return "mobile";
+};
+
+const getMaxBarHeight = () => {
+  const device = getDevice();
+  if (device == "web") return 125;
+  if (device == "mobile") return 150;
+};
+
+const getWeekday = () => {
+  const names = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+  const today = new Date();
+  return names[today.getDay()];
+};
 
 const setBarHeights = (data) => {
-    const maxAmount = data.reduce((acc, curr) => Math.max(acc, curr.amount), 0);
+  const maxAmount = data.reduce((acc, curr) => Math.max(acc, curr.amount), 0);
+  const maxBarHeight = getMaxBarHeight();
+  data.forEach((expense) => {
+    const { day, amount } = expense;
 
-    data.forEach((expense) => {
-      const { day, amount } = expense;
-      
-      const bar = document.getElementById(`bar-${day}`);
-      console.log(bar)
-      bar.style.height = `${(200 * amount) / maxAmount}px`;
+    const bar = document.getElementById(`bar-${day}`);
+    bar.style.height = `${(maxBarHeight * amount) / maxAmount}px`;
+  });
+};
+
+const setBarAmounts = (data) => {
+  data.forEach((expense) => {
+    const { day, amount } = expense;
+
+    const container = document.getElementById(`amount-${day}`);
+    container.innerText = `$${amount}`;
+  });
+};
+
+const setBarListener = () => {
+  const bars = Array.from(document.getElementsByClassName("bar"));
+  bars.forEach((bar) => {
+    const weekday = bar.id.split("-")[1];
+
+    bar.addEventListener("click", () => {
+      document.getElementById(`amount-${weekday}`).classList.toggle("hidden");
     });
-}
+
+  });
+};
+
+const setTodayBar = () => {
+  const weekday = getWeekday();
+  const bar = document.getElementById(`bar-${weekday}`);
+  bar.classList.add("today");
+};
 window.onload = async () => {
   const data = await getData();
-  setBarHeights(data)
+  setBarAmounts(data);
+  setBarHeights(data);
+  setBarListener();
+  setTodayBar();
 };
